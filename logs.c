3a77dd6f583d2c7eda751ad2cadc6681c7fe6329
John Kernagis <jkernagis2@gmail.com>
2013-09-10 05:03:13 +0200

    Logs generation works 100% as far as I can tell

--- logs.c
+++ logs.c
@@ -17,11 +17,11 @@ int main(){
     printf("How many lines to generate? :::> ");
     scanf("%d",&num_lines);
     printf("What machine am I? :::> ");
-    scanf("%d",&machine_num);
+    scanf("%d",&machine_num);    
 
     buffer[13] = '\0';
     sprintf(buffer,"machine.%d.log",machine_num);
-    FILE* log = fopen(buffer,"w");
+    FILE* fp = fopen(buffer,"w");
     
     fprintf(fp, "[SYSTEM]::LOGGING_SYSTEM_ONLINE\n");
     
@@ -31,27 +31,40 @@ int main(){
             
             case 0:
                 fprintf(fp, "%s%d%s", "[INFO]::USER_", rand()%200, "::LOGON\n");
+                break;
             case 1:
                 fprintf(fp, "%s%d%s", "[INFO]::USER_", rand()%200, "::LOGOFF\n");
+                break;
             case 2:
                 fprintf(fp, "%s%d%s", "[ERROR]::USER_", rand()%200, "::INVALID_REQUEST\n");
+                break;
             case 3:
                 fprintf(fp, "%s%d%s", "[ERROR]::USER_", rand()%200, "::CONNECTION_TIMEOUT\n");
+                break;
             case 4:
                 fprintf(fp, "%s%d%s", "[ERROR]::USER_", rand()%200, "::SERVER_OVERLOADED\n");
+                break;
             case 5:
                 fprintf(fp, "%s%d%s", "[WARNING]::USER_", rand()%200, "::INVALID_PASSWORD\n");
+                break;
             case 6:
                 fprintf(fp, "%s%d%s", "[WARNING]::USER_", rand()%200, "::LOGON_ATTEMPTS_EXCEEDED\n");
+                break;
             case 7:
                 fprintf(fp, "%s%d%s", "[WARNING]::USER_", rand()%200, "::OS_OUT_OF_DATE\n");
+                break;
             case 8:
                 fprintf(fp, "%s%d%s", "[INTRUSION]::USER_", rand()%200, "::ACCOUNT_COMPROMISED\n");
+                break;
             case 9:
                 fprintf(fp, "%s%d%s", "[QUERY]::USER_", rand()%200, "::QUERY_RECIEVED\n");
+                break;
+            default:
+                fprintf(fp, "%s%d%s", "[SYSTEM]::WHAT_HAPPENED\n");
+                break;
         }
     }
-    fprintf(fp, "[SYSTEM]::SYSTEM_SHUTDOWN\n"
+    fprintf(fp, "[SYSTEM]::SYSTEM_SHUTDOWN\n");
     fputc('\n',fp);
     fclose(fp);
 }
