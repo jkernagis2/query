cb0444427a9c6e3e9e97b3e9bc782903f43340bf
John Kernagis <jkernagis2@gmail.com>
2013-09-14 16:39:54 -0500

    fixed stupid mistakes with setting the value of array objects

--- query.c
+++ query.c
@@ -13,22 +13,22 @@
 #include "logs.c"
 
 //#define SERVER_HARD_ADDRESS "127.0.0.1"
-#define GREP_TEST_TARGET 
+#define GREP_TEST_TARGET
 
 void d_grep(char* command_buffer);
 
 int main(){
     // Currently calls the log generator, then grep's it for
-    
+
     int machine_num;
-    
+
     printf("What machine am I? :::> ");
     scanf("%d",&machine_num);
     gen_logs(machine_num);
-    
+
     // Now we do grep/gawk on the file
     d_grep("grep -k WARNING");
-    
+
     return 0;
 
 }
@@ -41,10 +41,10 @@ void d_grep(char* command_buffer) {
     int sys_str_size;                 // Size of search string alone
     char* gawk_buffer;            // Buffer
     char* system_buffer;          // Buffer
-    
+
     // Check for flags, hyphen would be after "grep "
-    if(command_buffer[6] == "-"){
-        switch(command_buffer[7]){
+    if(*command_buffer[6] == "-"){
+        switch(*command_buffer[7]){
             case "k":
                 command_flag = 1;
                 break;
@@ -66,22 +66,22 @@ void d_grep(char* command_buffer) {
     system_buffer = malloc(52 + search_str_size) * sizeof(char));
     // Copy over the search expression
     j = 0;
-    while(command_buffer[i] != "\0"){
-        gawk_buffer[j] = command_buffer[i];
+    while(*command_buffer[i] != "\0"){
+        *gawk_buffer[j] = *command_buffer[i];
         i++;
         j++;
     }
-    
+
     // Generating console command string
     // 14 + search string size + 38 = Size of string output to system
     sprintf(system_buffer,"%s%s%s","gawk -F: '$0~/",gawk_buffer,"/{print$0}' machine.1.log > result.tmp");
-    
+
     // Setting whether we want to search the whole line/key/values
-    system_buffer[12] = command_flag;
-    
+    *system_buffer[12] = command_flag;
+
     // Make the system call to do gawk
     system(system_buffer);
-    
+
     // Results of the gawk are in results.tmp, ready to be sent out
     return;
 }
