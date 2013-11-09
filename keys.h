fe1033190cd6aeafe8c5c2f8d6695a17f98e8b0a
John Kernagis <jkernagis2@gmail.com>
2013-11-08 19:36:10 -0600

    Adding new files for keyval stuff.

---
+++ keys.h
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+//                              keys.h                                  //
+//  Includes and declarations for the keys.c file.                      //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef KEYS_H
+#define KEYS_H
+
+#include "shared.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+#endif // KEYS_H
+
