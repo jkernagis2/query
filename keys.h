3a59d6d10e0d1edd4eb98e6822ac78e3a1273ce0
John Kernagis <jkernagis2@gmail.com>
2013-11-08 21:15:56 -0600

    I have no idea what's different

--- keys.h
+++ keys.h
@@ -4,7 +4,7 @@
 //  Includes and declarations for the keys.c file.                      //
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
-
+(insert, lookup, update, delete)
 #ifndef KEYS_H
 #define KEYS_H
 
@@ -12,7 +12,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+extern keyval* mykv;
+extern ring* myring;
 
+void local_insert(int key, char* val);
+
+char* local_lookup(int key);
+
+void local_update(int key, char* new_val);
+
+void local_delete(int key);
+
+void local_show();
 
 #endif // KEYS_H
-
